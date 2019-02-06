82178e3a4d916ab900837eed912fd3dabe1e9fda
Romain <rmtestard@gmail.com>
2019-02-06 20:25:05 +0100

    using ltrace to see the logic inside a file

--- leviathan.sh
+++ leviathan.sh
@@ -1,8 +1,17 @@
 lvl 0 --> lvl 1
-ssh leviathan0@leviathan.labs.overthrewire.org -p 2223
+ssh leviathan0@leviathan.labs.overthewire.org -p 2223
 leviathan0
 ls -a
 cd .backup/
 ls
 cat bookmarks.html | grep -i 'password'
 rioGegei8m
+
+lvl 1 --> lvl 2
+ssh leviathan1@leviathan.labs.overthewire.org -p 2223
+ls
+./check
+ltrace ./check 
+// we see this line strcmp("hel", "sex") // the password is probably sex, we obtain a bash
+cat /etc/leviathan_pass/leviathan2
+ougahZi8Ta
